2018e180d205771abfb0ea44802d4f09f7362bff
lukabuz <buzaladzeluka@gmail.com>
2019-10-27 15:00:00 -0400

    finished task 8 - functions

---
+++ 7_functions/js/myScript.js
@@ -0,0 +1,15 @@
+setElementValue = (elementId, value) => {
+	let element = document.getElementById(elementId);
+	element.innerHTML = value;
+	return;
+};
+
+functionOne = () => {
+	return "Text returned from function one";
+};
+functionTwo = () => {
+	return "Text returned from function two";
+};
+functionThree = () => {
+	return "Text returned from function three";
+};
